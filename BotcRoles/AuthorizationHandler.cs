26c1a94696035e856805a71a5379f66db84e7c0a
Strapontin <antodu73@hotmail.fr>
2023-08-25 15:22:15 +0200

    Cache user bearer to not spam discord api

--- BotcRoles/AuthorizationHandler.cs
+++ BotcRoles/AuthorizationHandler.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.Net.Http.Headers;
 using System.Text.Json;
-using System.Web.Http.Controllers;
-using System.Web.Http.Filters;
 
 namespace BotcRoles
 {
@@ -15,11 +13,12 @@ namespace BotcRoles
     public class IsStoryTellerAuthorizationHandler : AuthorizationHandler<IsStoryTellerRequirement>
     {
         const string _tbaServerId = "765137571608920074";
-        const string storyTellerRoleId = "797739056406069279";
+        const string _storyTellerRoleId = "797739056406069279";
+        List<BearerStoryTeller> _bearerIsStoryTeller = new();
 
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, IsStoryTellerRequirement requirement)
         {
-            string? bearer = ((DefaultHttpContext)context.Resource).Request?.Headers[HeaderNames.Authorization].FirstOrDefault();
+            string? bearer = (context.Resource as DefaultHttpContext).Request?.Headers[HeaderNames.Authorization].FirstOrDefault();
 
             if (string.IsNullOrWhiteSpace(bearer))
             {
@@ -27,58 +26,109 @@ namespace BotcRoles
                 return;
             }
 
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get,
-                $"https://discord.com/api/users/@me/guilds/{_tbaServerId}/member")
+            List<string> bearerExpired = new();
+            foreach (var b in _bearerIsStoryTeller)
             {
-                Headers = { { HeaderNames.Authorization, bearer } }
-            };
-
-            var httpClient = new HttpClient();
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                var contentStream = await httpResponseMessage.Content.ReadAsStringAsync();
-                var userGuildDetails = JsonSerializer.Deserialize<UserGuildDetails>(contentStream);
-
-                if (userGuildDetails == null || userGuildDetails.user == null)
+                if (DateTime.Now.Subtract(b.DateChecked).TotalMinutes >= 10)
                 {
-                    context.Fail();
-                    return;
-                }
-
-                if (userGuildDetails.roles.Contains(storyTellerRoleId))
-                {
-                    context.Succeed(requirement);
-                    return;
+                    bearerExpired.Add(b.Bearer);
                 }
             }
+            _bearerIsStoryTeller.RemoveAll(b => bearerExpired.Contains(b.Bearer));
 
-            context.Fail();
-            return;
+            // If the user has already authenticated 
+            if (_bearerIsStoryTeller.Any(b => b.Bearer == bearer))
+            {
+                ResolveContext(context, requirement, bearer);
+                return;
+            }
+            else
+            {
+                bool isUserStoryTeller = false;
+
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get,
+                    $"https://discord.com/api/users/@me/guilds/{_tbaServerId}/member")
+                {
+                    Headers = { { HeaderNames.Authorization, bearer } }
+                };
+
+                var httpClient = new HttpClient();
+                var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    var contentStream = await httpResponseMessage.Content.ReadAsStringAsync();
+                    var userGuildDetails = JsonSerializer.Deserialize<UserGuildDetails>(contentStream);
+
+                    if (userGuildDetails == null || userGuildDetails.user == null)
+                    {
+                        isUserStoryTeller = false;
+                        context.Fail();
+                    }
+                    else if (userGuildDetails.roles.Contains(_storyTellerRoleId))
+                    {
+                        isUserStoryTeller = true;
+                        context.Succeed(requirement);
+                    }
+                }
+                else
+                {
+                    context.Fail();
+                }
+
+                _bearerIsStoryTeller.Add(new(bearer, isUserStoryTeller, DateTime.Now));
+                return;
+            }
+        }
+
+        private void ResolveContext(AuthorizationHandlerContext context, IsStoryTellerRequirement requirement, string bearer)
+        {
+            // If user has the rights
+            if (_bearerIsStoryTeller.Find(b => b.Bearer == bearer).IsStoryTeller)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+        }
+
+
+        private class BearerStoryTeller
+        {
+            public BearerStoryTeller(string bearer, bool isStoryTeller, DateTime datechecked)
+            {
+                Bearer = bearer;
+                IsStoryTeller = isStoryTeller;
+                DateChecked = datechecked;
+            }
+
+            public string Bearer { get; set; }
+            public bool IsStoryTeller { get; set; }
+            public DateTime DateChecked { get; set; }
+        }
+
+        private class UserGuildDetails
+        {
+            public string[] roles { get; set; }
+            public DiscordUser user { get; set; }
+        }
+
+        private class DiscordUser
+        {
+            public string id { get; set; }
+            public string? username { get; set; }
+            public string? avatar { get; set; }
+            public string? discriminator { get; set; }
+            public int public_flags { get; set; }
+            public int flags { get; set; }
+            public string? banner { get; set; }
+            public string? accent_color { get; set; }
+            public string? global_name { get; set; }
+            public string? avatar_decoration { get; set; }
+            public string? display_name { get; set; }
+            public string? banner_color { get; set; }
         }
     }
-}
-
-
-public class UserGuildDetails
-{
-    public string[] roles { get; set; }
-    public DiscordUser user { get; set; }
-}
-
-public class DiscordUser
-{
-    public string id { get; set; }
-    public string? username { get; set; }
-    public string? avatar { get; set; }
-    public string? discriminator { get; set; }
-    public int public_flags { get; set; }
-    public int flags { get; set; }
-    public string? banner { get; set; }
-    public string? accent_color { get; set; }
-    public string? global_name { get; set; }
-    public string? avatar_decoration { get; set; }
-    public string? display_name { get; set; }
-    public string? banner_color { get; set; }
 }
