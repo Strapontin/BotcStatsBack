362708ea339a8b991fab22af0a57e6aa674d5f17
Strapontin <antodu73@hotmail.fr>
2023-11-01 19:10:25 +0100

    Changed date format in DB

--- BotcRoles/Helper/Converter.cs
+++ BotcRoles/Helper/Converter.cs
@@ -12,13 +12,33 @@ namespace BotcRoles.Helper
                 v => v ? 1 : 0,
                 v => v == 1);
         }
+
         public static ValueConverter GetConverterDateTimeToString()
         {
-            var culture = new CultureInfo("en-US");
-
             return new ValueConverter<DateTime, string>(
-                v => v.ToString(culture),
-                v => DateTime.Parse(v, culture));
+                v => v.ToString("yyyy-MM-dd HH:mm:ss"),
+                v => GetDateFromString(v));
+        }
+
+        private static DateTime GetDateFromString(string v)
+        {
+            try
+            {
+                int year = int.Parse(v.Substring(0, 4));
+                int month = int.Parse(v.Substring(5, 2));
+                int day = int.Parse(v.Substring(8, 2));
+                int hour = int.Parse(v.Substring(11, 2));
+                int minute = int.Parse(v.Substring(14, 2));
+                int second = int.Parse(v.Substring(17, 2));
+
+                return new DateTime(year, month, day, hour, minute, second);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error on Date conversion ! ", e.Message);
+                return new DateTime();
+            }
         }
     }
 }
+
