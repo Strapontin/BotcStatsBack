e06747fc379e750487235ad125b93d85f82be241
Strapontin <antodu73@hotmail.fr>
2024-03-24 15:25:46 +0100

    Updated linux file

--- DbBackupMaker/linux.py
+++ DbBackupMaker/linux.py
@@ -1,16 +1,23 @@
+#! /usr/bin/python3
+
+# In Linux > Startup Applications : Create Command "gnome-terminal -- python3 /home/anthony/Documents/Dev/BotcStatsBack/DbBackupMaker/linux.py"
+
 import os
-import platform
 from datetime import datetime
 import subprocess
+import sys
+
+
+secretsFilePath = f"{sys.path[0]}/secrets"
 
 # If file doesn't exists
-if not os.path.exists("./secrets"):
+if not os.path.exists(secretsFilePath):
     print(f"No secrets file found at {os.path.abspath(os.path.curdir)}")
     input()
     exit()
 
 mySecrets = {}
-with open("./secrets", "r") as file:
+with open(secretsFilePath, "r") as file:
     for line in file:
         name, var = line.partition("=")[::2]
         mySecrets[name.strip()] = var.strip()
@@ -29,14 +36,14 @@ if os.path.exists(backup_file):
 try:
     print("Backuping...")
     print("\n")
-    
+
     subprocess.run(f"export PGPASSWORD={mySecrets['PASSWORD']}\n" +
                    f"pg_dump " +
                    f"-h {mySecrets['HOST']} " +
                    f"-U {mySecrets['USER']} " +
-                   f"-d {mySecrets['DATABASE']} " + 
+                   f"-d {mySecrets['DATABASE']} " +
                    f"-Fc -f {backup_file}", shell=True, check=True)
-    
+
     print(f"\nBackup completed successfully. Backup file is {backup_file}")
 except subprocess.CalledProcessError as ex:
     print("\nError during backup:", ex)
